25758d920cdf33424653b59e589a22628d7f151a
Rafael <rafaeloliveiracds@gmail.com>
2024-01-15 15:13:41 -0300

    [ Feat ] List read all

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,3 +1,7 @@
+# Todo
+# update
+# delete
+
 class ListController < ApplicationController
   def create
     list = List.new(list_params)
@@ -5,6 +9,11 @@ class ListController < ApplicationController
     render json: { list:, user: list.user }, status: :ok
   rescue StandardError
     render json: list.errors, status: :unprocessable_entity
+  end
+
+  def index
+    lists = List.where(user_id: params.require(:user_id))
+    render json: { lists:, total: lists.length }, status: :ok
   end
 
   private
