c5ce3cc380db49b1bb82f4be79b4d533e5f53e19
Rafael <rafaeloliveiracds@gmail.com>
2024-01-18 13:14:18 -0300

    Add dependent to user and list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,6 @@
 class List < ApplicationRecord
   belongs_to :user
-  has_many :recipes
-
+  has_many :recipes, dependent: :destroy
   validates :user_id, presence: true
   validates :title, length: { minimum: 3, maximum: 30 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :lists
+  has_many :lists, dependent: :destroy
   validates :email, uniqueness: true, presence: true
   validates :name, length: { minimum: 3 }
   validates :password, length: { minimum: 10 }, presence: true
