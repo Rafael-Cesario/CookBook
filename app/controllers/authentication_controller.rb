b64ec4f9aaa43baaa6645156b6758593e07333d0
Rafael <rafaeloliveiracds@gmail.com>
2024-01-25 12:31:46 -0300

    Login params

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,14 +4,18 @@ class AuthenticationController < ApplicationController
   rescue_from AuthenticationError, with: :invalid_credentials
 
   def login
-    user = User.find_by(email: params.require(:email))
+    user = User.find_by(email: login_params[:email])
     raise AuthenticationError unless user
 
-    valid_password = user.authenticate(params.require(:password))
+    valid_password = user.authenticate(login_params[:password])
     raise AuthenticationError unless valid_password
 
     token = AuthenticationService.generate_token(user.email)
     render json: { token:, email: user.email, name: user.name }
+  end
+
+  def login_params
+    params.require(:authentication).permit(:email, :password)
   end
 
   def invalid_credentials
