5719dba1078b68bb0a7a8bc0733550f99ffbe0a5
Rafael <rafaeloliveiracds@gmail.com>
2024-02-20 14:19:54 -0300

    Downcase user model before validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password, length: { in: 10..30 }
   validates :name, length: { maximum: 20 }
+
+  before_validation :downcase_fields
+
+  def downcase_fields
+    self.email.downcase!
+    self.name.downcase!
+  end
 end
