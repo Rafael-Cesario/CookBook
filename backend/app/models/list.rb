7a0357d5223d828ad0232685ba0e9444ea024425
Rafael <rafaeloliveiracds@gmail.com>
2024-02-22 19:42:59 -0300

    List model

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
-
+  belongs_to :user, dependent: :destroy
+  validates :user_id, :title, presence: true
+  validates :title, length: { maximum: 100 }
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :lists
   has_secure_password
   validates :email, :password, :name, presence: true
   validates :email, uniqueness: true
