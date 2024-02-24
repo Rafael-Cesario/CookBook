2267b7e8e83a0a38b4ff6810320cb5e22b847882
Rafael <rafaeloliveiracds@gmail.com>
2024-02-23 21:08:24 -0300

    [ Feat ] List controller update

--- backend/app/controllers/api/list_controller.rb
+++ backend/app/controllers/api/list_controller.rb
@@ -1,4 +1,6 @@
 class Api::ListController < ApplicationController
+  NOT_FOUND_ERROR =  "notFound: A list with id: %s was not found."
+
   before_action :authorize
 
   def create
@@ -12,6 +14,18 @@ class Api::ListController < ApplicationController
   def index
     lists = List.where(user_id: params.require(:user_id))
     render json: { lists:, total: lists.length }
+  end
+
+  def update
+    list = List.find_by(id: params.require(:id))
+    raise StandardError.new NOT_FOUND_ERROR % params[:id] unless list
+
+    list.update(title: params.require(:title))
+    raise StandardError.new list.errors.first.full_message unless list.valid?
+
+    render json: list, status: :ok
+  rescue => error
+    render json: { error: error.message }, status: :unprocessable_entity
   end
 
   private
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :user, only: [:create]
-    resources :list, only: [:create, :index]
+    resources :list, only: [:create, :index, :update]
     post "/authentication", to: "authentication#login"
   end
 end
