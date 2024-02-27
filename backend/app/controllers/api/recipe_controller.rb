1f37dbbc8606b315e73b2b3b3c9954a4bf122ccb
Rafael <rafaeloliveiracds@gmail.com>
2024-02-27 20:04:46 -0300

    [ Feat ] Recipe controller update

--- backend/app/controllers/api/recipe_controller.rb
+++ backend/app/controllers/api/recipe_controller.rb
@@ -1,6 +1,8 @@
 class Api::RecipeController < ApplicationController
   before_action :authorize
 
+  NOT_FOUND_ERROR =  "notFound: A recipe with id: %s was not found."
+
   def create
     recipe = Recipe.new(recipe_params)
     recipe.save!
@@ -12,6 +14,19 @@ class Api::RecipeController < ApplicationController
   def index
     recipes = Recipe.where(list_id: params.require(:list_id))
     render json: { recipes:, total: recipes.length }, status: :ok
+  end
+
+  def update
+    recipe = Recipe.find_by(id: params.require(:id))
+    raise StandardError.new NOT_FOUND_ERROR % params[:id] unless recipe
+
+    recipe.update(recipe_params)
+    errors = recipe.errors.full_messages.join(", ")
+    raise StandardError.new errors unless recipe.valid?
+
+    render json: { recipe:, message: "Success: Recipe updated" }
+  rescue => error
+    render json: { errors: error.message }, status: :unprocessable_entity
   end
 
   private
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :user, only: [:create]
     resources :list, only: %i[create index update destroy]
-    resources :recipe, only: %i[create index]
+    resources :recipe, only: %i[create index update]
     post '/authentication', to: 'authentication#login'
   end
 end
