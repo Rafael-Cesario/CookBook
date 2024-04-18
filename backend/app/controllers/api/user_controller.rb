abcfcbbdf8d6bce1f2d35e74901969a4d7bb887a
Rafael <rafaeloliveiracds@gmail.com>
2024-04-18 20:37:53 -0300

    [ Feat ] Create user

--- backend/app/controllers/api/user_controller.rb
+++ backend/app/controllers/api/user_controller.rb
@@ -1,5 +1,18 @@
 class Api::UserController < ApplicationController
+  class UserError < StandardError; end
+
   def create
-    render json: { message: "User controller" }, status: :ok
+    user = User.new(user_params)
+    raise UserError unless user.save
+    response = { id: user[:id], email: user[:email], message: "A new user was created with success" }
+    render json: response, status: :ok
+  rescue
+    render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
   end
 end
