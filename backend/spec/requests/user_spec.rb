9d87b1088dac5d60b1a9064e527bab7ff15540c8
Rafael <rafaeloliveiracds@gmail.com>
2024-04-23 08:40:50 -0300

    Rubocop

--- backend/spec/requests/user_spec.rb
+++ backend/spec/requests/user_spec.rb
@@ -11,11 +11,12 @@ RSpec.describe 'Users', type: :request do
 
     it 'Creates a new user' do
       post path, params: { user: user_data }
-      expect(json).to include({
-                                id: be_an_instance_of(String),
-                                email: user_data[:email].downcase,
-                                message: be_an_instance_of(String)
-                              })
+      fields = {
+        id: be_an_instance_of(String),
+        email: user_data[:email].downcase,
+        message: be_an_instance_of(String)
+      }
+      expect(json).to include(fields)
     end
 
     it 'Has a secure password' do
@@ -25,26 +26,26 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'Validates fields presence' do
-      post path, params: { user: { email: "", password: "", name: "" }}
-      errors = [ "Email can't be blank", "Password can't be blank", "Name can't be blank" ]
+      post path, params: { user: { email: '', password: '', name: '' } }
+      errors = ["Email can't be blank", "Password can't be blank", "Name can't be blank"]
       expect(json[:errors]).to include(*errors)
     end
 
     it 'Validates email uniqueness' do
       post path, params: { user: user_data }
       post path, params: { user: user_data }
-      expect(json[:errors]).to include "Email has already been taken"
+      expect(json[:errors]).to include 'Email has already been taken'
     end
 
     it 'Validates name length' do
-      name =  Faker::Lorem.characters(number: 31)
-      post path, params: { user: { **user_data, name: }}
-      expect(json[:errors]).to include "Name is too long (maximum is 30 characters)"
+      name = Faker::Lorem.characters(number: 31)
+      post path, params: { user: { **user_data, name: } }
+      expect(json[:errors]).to include 'Name is too long (maximum is 30 characters)'
     end
 
     it 'Downcase a email before save' do
-      email = "HELLO@EMAIL.COM"
-      post path, params: { user: { **user_data, email: }}
+      email = 'HELLO@EMAIL.COM'
+      post path, params: { user: { **user_data, email: } }
       user = User.find_by(email: email.downcase)
 
       expect(json[:email]).to eq email.downcase
