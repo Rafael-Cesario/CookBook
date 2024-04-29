c824bfbbede14e0500e32d8a35c07e42d24193e1
Rafael <rafaeloliveiracds@gmail.com>
2024-04-29 15:38:00 -0300

    [ Feat ] Update input values

--- frontend/src/features/authentication/components/field.tsx
+++ frontend/src/features/authentication/components/field.tsx
@@ -5,17 +5,18 @@ interface IField {
 		label: string;
 		field: string;
 		type: "text" | "password";
+		onChange(value: string): void;
 	};
 }
 
-export const Field = ({ props: { field, label, type } }: IField) => {
+export const Field = ({ props: { field, label, type, onChange } }: IField) => {
 	return (
 		<StyledField>
 			<label className="text" htmlFor={`#${field}`}>
 				{label}
 			</label>
 
-			<input type={type} id={field} />
+			<input onChange={(e) => onChange(e.target.value)} type={type} id={field} />
 		</StyledField>
 	);
 };
--- frontend/src/features/authentication/create-user-form.tsx
+++ frontend/src/features/authentication/create-user-form.tsx
@@ -1,16 +1,66 @@
+"use client";
+
+import { useState } from "react";
 import { Field } from "./components/field";
 import { StyledCreateUserForm } from "./styles/create-user-form";
+import { produce } from "immer";
 
 export const CreateUserForm = () => {
+	const userDefaultValues = { email: "", name: "", password: "", passwordValidation: "" };
+
+	const [userData, setUserData] = useState({ ...userDefaultValues });
+	const [dataErrors, setDataErrors] = useState({ ...userDefaultValues });
+
+	const createUser = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log({ userData, dataErrors });
+	};
+
+	const updateValues = (field: keyof typeof userDefaultValues, value: string) => {
+		const nextState = produce(userData, (draft) => void (draft[field] = value));
+		setUserData(nextState);
+	};
+
 	return (
 		<StyledCreateUserForm>
 			<h1 className="title">Criar uma Conta</h1>
 
-			<form>
-				<Field props={{ field: "email", label: "Email", type: "text" }} />
-				<Field props={{ field: "name", label: "Nome", type: "text" }} />
-				<Field props={{ field: "password", label: "Senha", type: "password" }} />
-				<Field props={{ field: "passwordValidation", label: "Confirme sua senha", type: "password" }} />
+			<form onSubmit={(e) => createUser(e)}>
+				<Field
+					props={{
+						onChange: (value) => updateValues("email", value),
+						field: "email",
+						label: "Email",
+						type: "text",
+					}}
+				/>
+
+				<Field
+					props={{
+						onChange: (value) => updateValues("name", value),
+						field: "name",
+						label: "Nome",
+						type: "text",
+					}}
+				/>
+
+				<Field
+					props={{
+						onChange: (value) => updateValues("password", value),
+						field: "password",
+						label: "Senha",
+						type: "password",
+					}}
+				/>
+
+				<Field
+					props={{
+						onChange: (value) => updateValues("passwordValidation", value),
+						field: "passwordValidation",
+						label: "Confirme sua senha",
+						type: "password",
+					}}
+				/>
 
 				<button className="submit">Confirmar</button>
 			</form>
