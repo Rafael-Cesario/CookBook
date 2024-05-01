239031810fd6a058acd357d1a06d0e68c872daab
Rafael <rafaeloliveiracds@gmail.com>
2024-05-01 13:27:28 -0300

    [ Feat ] Validate fields

--- frontend/src/features/authentication/create-user-form.tsx
+++ frontend/src/features/authentication/create-user-form.tsx
@@ -13,12 +13,35 @@ export const CreateUserForm = () => {
 
 	const createUser = (e: React.FormEvent) => {
 		e.preventDefault();
+
 		console.log({ userData, dataErrors });
+
+		const { errors, hasErrors } = validateFields();
+		if (hasErrors) return updateErrors(errors);
 	};
 
 	const updateValues = (field: keyof typeof userDefaultValues, value: string) => {
 		const nextState = produce(userData, (draft) => void (draft[field] = value));
 		setUserData(nextState);
+	};
+
+	const updateErrors = (errors: typeof userDefaultValues) => {
+		setDataErrors(errors);
+	};
+
+	// Todo >
+	const validateFields = () => {
+		const fields = Object.entries(userData);
+		const errors = { ...userDefaultValues };
+		let hasErrors = false;
+
+		fields.forEach(([key, value]) => {
+			// Validate field
+			// errors[key] = error
+			// if error hasErrors = true;
+		});
+
+		return { hasErrors, errors };
 	};
 
 	return (
